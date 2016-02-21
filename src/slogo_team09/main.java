8c7988e530219cb6f8f518b1f29b48ed40fce12b
Raghav <raghav.kedia@duke.edu>
2016-02-21 12:09:04 -0500

    created project in eclipse and added main.java

---
+++ src/slogo_team09/main.java
@@ -0,0 +1,23 @@
+package slogo_team09;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main extends Application{
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
