aa2e72c4bac0929deb0603cac851df529bd54d7a
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-23 23:01:41 -0500

    Testing making an expression tree with binary and constant expressions.

---
+++ src/slogo_team09/BinaryCommandNode.java
@@ -0,0 +1,50 @@
+package slogo_team09;
+
+import slogo_team09.Command;
+import slogo_team09.ExpressionNode;
+
+public class BinaryCommandNode implements ExpressionNode {
+	Command type;
+	ExpressionNode leftChild;
+	ExpressionNode rightChild;
+	
+	public BinaryCommandNode(Command type) {
+		this.type = type;
+	}
+	
+	@Override
+	public double execute() {
+		double leftValue = leftChild.execute();
+		double rightValue = rightChild.execute();
+		switch(type) {
+			case SUM:
+				return leftValue + rightValue;
+			case DIFFERENCE:
+				return leftValue - rightValue;
+			case PRODUCT:
+				return leftValue * rightValue;
+			case QUOTIENT:
+				return leftValue / rightValue;
+			case REMAINDER:
+				return leftValue % rightValue;
+			case POW:
+				return Math.pow(leftValue, rightValue);
+			case LESS:
+				return leftValue < rightValue ? 1 : 0;
+			case GREATER:
+				return leftValue > rightValue ? 1 : 0;
+			case EQUAL:
+				return leftValue == rightValue ? 1 : 0;
+			case NOTEQUAL:
+				return leftValue != rightValue ? 1 : 0;
+			case AND:
+				return ((leftValue != 0) && (rightValue != 0)) ? 1 : 0;
+			case OR:
+				return ((leftValue != 0) || (rightValue != 0)) ? 1 : 0;
+			default:
+				break;
+		}
+		return 0;
+	}
+
+}
---
+++ src/slogo_team09/testExpressionTree.java
@@ -0,0 +1,24 @@
+package slogo_team09;
+
+import slogo_team09.BinaryCommandNode;
+import slogo_team09.Command;
+import slogo_team09.ConstantNode;
+
+public class testExpressionTree {
+
+	public testExpressionTree() {
+
+	}
+	
+	public static void main(String[] args) {
+		BinaryCommandNode root = new BinaryCommandNode(Command.SUM);
+		root.leftChild = new BinaryCommandNode(Command.PRODUCT);
+		BinaryCommandNode leftleftChild = (BinaryCommandNode) root.leftChild;
+		root.rightChild = new ConstantNode(1);
+		leftleftChild.leftChild = new ConstantNode(2);
+		leftleftChild.rightChild = new ConstantNode(1);
+		double result = root.execute();
+		System.out.println(result);
+	}
+
+}
