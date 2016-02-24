cdb96c8203f0e6611605d3a5a1237ef7491d6fa4
Richard Liu <richardliu7896@gmail.com>
2016-02-23 23:15:09 -0500

    added basic interfaces

--- src/frontend/FrontendManager.java
+++ src/frontend/FrontendManager.java
@@ -1,8 +1,6 @@
 package frontend;
 
 public class FrontendManager {
-	/* Use case
-	Button myB = ComponentFactory.makeNewButton(600, 600);
-	myB.bind(100, 100);
-	*/
+//	Button myB = ComponentFactory.makeNewButton(600, 600);
+//	myB.bind(100, 100);
 }
---
+++ src/frontend/IDisplayable.java
@@ -0,0 +1,5 @@
+package frontend;
+
+interface IDisplayable {
+	void display();
+}
