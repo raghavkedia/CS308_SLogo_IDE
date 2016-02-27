323875ccba0aab0c8366b4690e73b47c52302255
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-27 14:15:47 -0500

    Updated parseable.

--- src/backend/Parseable.java
+++ src/backend/Parseable.java
@@ -1,7 +1,10 @@
 package backend;
 
+import java.util.Collection;
+
 public interface Parseable {
 	
 	public ParsedInput parse(String input);
+	public Collection<String> stringParse(String input);
 	
 }
