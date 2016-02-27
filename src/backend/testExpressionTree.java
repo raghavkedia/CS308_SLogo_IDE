f8105f8a564eca5d9c91a435a3e05328f625fc74
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-27 14:27:29 -0500

    Modified test to run with command nodes.

--- src/backend/testExpressionTree.java
+++ src/backend/testExpressionTree.java
@@ -11,15 +11,20 @@ public class testExpressionTree {
 	}
 	
 	public static void main(String[] args) {
-		BinaryCommandNode root = new BinaryCommandNode(Command.PRODUCT);
-		root.leftChild = new BinaryCommandNode(Command.SUM);
-		BinaryCommandNode leftleftChild = (BinaryCommandNode) root.leftChild;
-		root.rightChild = new ConstantNode(1);
-		ConstantNode c = new ConstantNode(2);
-		leftleftChild.leftChild = c;
-		leftleftChild.rightChild = new ConstantNode(1);
-		Result result = root.execute();
-		System.out.println(result.getMyDouble());
+		CommandNode root = new CommandNode(Command.PRODUCT);
+		CommandNode newChild = new CommandNode(Command.SUM);
+		root.addChild(newChild);
+		root.addChild(new ConstantNode(1));
+		newChild.addChild(new ConstantNode(2));
+		newChild.addChild(new ConstantNode(1));
+		double result = root.execute();
+//		BinaryCommandNode leftleftChild = (BinaryCommandNode) root.leftChild;
+//		root.rightChild = new ConstantNode(1);
+//		ConstantNode c = new ConstantNode(2);
+//		leftleftChild.leftChild = c;
+//		leftleftChild.rightChild = new ConstantNode(1);
+//		Result result = root.execute();
+		System.out.println(result);
 	}
 
 }
