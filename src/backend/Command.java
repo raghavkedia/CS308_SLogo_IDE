9a70dd6b16d505e4f09512067d8b49ca4fe6ca55
Christine Zhou <zhou.l.christine@gmail.com>
2016-02-27 22:58:23 -0500

    Correcting previous commands.

--- src/backend/Command.java
+++ src/backend/Command.java
@@ -4,5 +4,91 @@ public enum Command {
 	Forward, Back, Left, Right, SetHeading, SetTowards, SetPosition, PenDown, PenUp, ShowTurtle, HideTurtle, Home, ClearScreen,
 	XCoordinate, YCoordinate, Heading, IsPenDown, IsShowing, Sum, Difference, Product, Quotient, Remainder, Minus, Random, 
 	Sine, Cosine, Tangent, ArcTangent, NaturalLog, Power, Pi, LessThan, GreaterThan, Equal, NotEqual, And, Or, Not;
+
+	public int numArgs(Command type) {
+		switch(type) {
+			case Forward:
+				return 1;
+			case Back:
+				return 1;
+			case Left:
+				return 1;
+			case Right:
+				return 1;
+			case SetHeading:
+				return 1;
+			case SetTowards:
+				return 2;
+			case SetPosition:
+				return 2;
+			case PenDown:
+				return 0;
+			case PenUp:
+				return 0;
+			case ShowTurtle:
+				return 0;
+			case HideTurtle:
+				return 0;
+			case Home:
+				return 0;
+			case ClearScreen:
+				return 0;
+			case XCoordinate:
+				return 0;
+			case YCoordinate:
+				return 0;
+			case Heading:
+				return 0;
+			case IsPenDown:
+				return 0;
+			case IsShowing:
+				return 0;
+			case Sum:
+				return 2;
+			case Difference:
+				return 2;
+			case Product:
+				return 2;
+			case Quotient:
+				return 2;
+			case Remainder:
+				return 2;
+			case Minus:
+				return 1;
+			case Random:
+				return 1;
+			case Sine:
+				return 1;
+			case Cosine:
+				return 1;
+			case Tangent:
+				return 1;
+			case ArcTangent:
+				return 1;
+			case NaturalLog:
+				return 1;
+			case Power:
+				return 2;
+			case Pi:
+				return 0;
+			case LessThan:
+				return 2;
+			case GreaterThan:
+				return 2;
+			case Equal:
+				return 2;
+			case NotEqual:
+				return 2;
+			case And:
+				return 2;
+			case Or:
+				return 2;
+			case Not:
+				return 0;
+			default:
+				break;
+		}
+		return 0;
+	}
 }
 
--- src/backend/CommandFactory.java
+++ src/backend/CommandFactory.java
@@ -45,152 +45,155 @@ public class CommandFactory {
 		double leftValue = 0;
 		double rightValue = 0;
 		switch(type) {
-		case Forward:
-			translateCoor(new double[]{0, myResults.get(0)});
-			return myResults.get(0);
-		case Back:
-			translateCoor(new double[]{0, -1 * myResults.get(0)});
-			return myResults.get(0);
-		case Left:
-			myCharacter.setMyAngle(myCharacter.getMyAngle() - myResults.get(0));
-			myCharacter.hasUpdated();
-			return myResults.get(0);
-		case Right:
-			myCharacter.setMyAngle(myCharacter.getMyAngle() + myResults.get(0));
-			myCharacter.hasUpdated();
-			return myResults.get(0);
-		case SetHeading:
-			result = Math.abs(myCharacter.getMyAngle() - myResults.get(0));
-			myCharacter.setMyAngle(myResults.get(0));
-			myCharacter.hasUpdated();
-			return result;
-		case SetTowards:
-			double newAngle = 
-				90 - Math.atan2(myResults.get(1) - myCharacter.getCoordY(), myResults.get(0) - myCharacter.getCoordX());
-			result = Math.abs(newAngle - myCharacter.getMyAngle());
-			myCharacter.setMyAngle(newAngle);
-			myCharacter.hasUpdated();
-			return result;
-		case SetPosition:
-			result = findDistance(myResults.get(0), myCharacter.getCoordX(), myResults.get(1), myCharacter.getCoordY());
-			myCharacter.setCurrCoord((int) Math.round(myResults.get(0)), (int) Math.round(myResults.get(1)));
-			myCharacter.hasUpdated();
-			return result;
-		case PenDown:
-			myCharacter.setPenState(true);
-			myCharacter.hasUpdated();
-			return 1;
-		case PenUp:
-			myCharacter.hasUpdated();
-			myCharacter.setPenState(false);
-			return 0;
-		case ShowTurtle:
-			myCharacter.setVisability(true);
-			myCharacter.hasUpdated();
-			return 1;
-		case HideTurtle:
-			myCharacter.setVisability(false);
-			myCharacter.hasUpdated();
-			return 0;
-		case Home:
-			result = findDistance(0, myCharacter.getCoordX(), 0, myCharacter.getCoordY());
-			myCharacter.setCurrCoord(0, 0);
-			myCharacter.hasUpdated();
-			return result;
-		case ClearScreen:
-			//How to do this? for erasing all lines? deleting list?
-			break;
-		case XCoordinate:
-			return myCharacter.getCoordX();
-		case YCoordinate:
-			return myCharacter.getCoordY();
-		case Heading:
-			return myCharacter.getMyAngle();
-		case IsPenDown:
-			return myCharacter.getPenState() ? 1 : 0;
-		case IsShowing:
-			return myCharacter.getVisability() ? 1 : 0;
-		case Sum:
-			for (Double d : myResults) {
-				result += d;
-			}
-			return result;
-		case Difference:
-			result = myResults.get(0);
-			myResults.remove(0);
-			for (Double d : myResults) {
-				result -= d;
-			}
-			return result;
-		case Product:
-			result = myResults.get(0);
-			myResults.remove(0);
-			for (Double d : myResults) {
-				result = result * d;
-			}
-			return result;
-		case Quotient:
-			result = myResults.get(0);
-			myResults.remove(0);
-			for (Double d : myResults) {
-				result = result / d;
-			}
-			return result;
-		case Remainder:
-			result = myResults.get(0);
-			myResults.remove(0);
-			for (Double d : myResults) {
-				result = result % d;
-			}
-		case Minus:
-			return -1 * myResults.get(0);
-		case Random:
-			return Math.floor(Math.random() * myResults.get(0));
-		case Sine:
-			return Math.sin(convertDegrees(myResults.get(0)));
-		case Cosine:
-			return Math.cos(convertDegrees(myResults.get(0)));
-		case Tangent:
-			return Math.tan(convertDegrees(myResults.get(0)));
-		case ArcTangent:
-			//may throw exception here
-			return Math.atan(convertDegrees(myResults.get(0)));
-		case NaturalLog:
-			//may also throw exception here
-			return Math.log(myResults.get(0));
-		case Power:
-			double base = myResults.get(0); 
-			double exp = myResults.get(1);
-			return Math.pow(base, exp);
-		case Pi:
-			return Math.PI;
-		case LessThan:
-			leftValue =  myResults.get(0);
-			rightValue = myResults.get(1);
-			return (leftValue < rightValue ? 1 : 0);
-		case GreaterThan:
-			leftValue =  myResults.get(0);
-			rightValue = myResults.get(1);
-			return (leftValue > rightValue ? 1 : 0);
-		case Equal:
-			leftValue =  myResults.get(0);
-			rightValue = myResults.get(1);
-			return(leftValue == rightValue ? 1 : 0);
-		case NotEqual:
-			leftValue =  myResults.get(0);
-			rightValue = myResults.get(1);
-			return (leftValue != rightValue ? 1 : 0);
-		case And:
-			leftValue =  myResults.get(0);
-			rightValue = myResults.get(1);
-			return (((leftValue != 0) && (rightValue != 0)) ? 1 : 0);
-		case Or:
-			leftValue =  myResults.get(0);
-			rightValue = myResults.get(1);
-			return (((leftValue != 0) || (rightValue != 0)) ? 1 : 0);
-		default:
-			break;
-	}
+			case Forward:
+				translateCoor(new double[]{0, myResults.get(0)});
+				return myResults.get(0);
+			case Back:
+				translateCoor(new double[]{0, -1 * myResults.get(0)});
+				return myResults.get(0);
+			case Left:
+				myCharacter.setMyAngle(myCharacter.getMyAngle() - myResults.get(0));
+				myCharacter.hasUpdated();
+				return myResults.get(0);
+			case Right:
+				myCharacter.setMyAngle(myCharacter.getMyAngle() + myResults.get(0));
+				myCharacter.hasUpdated();
+				return myResults.get(0);
+			case SetHeading:
+				result = Math.abs(myCharacter.getMyAngle() - myResults.get(0));
+				myCharacter.setMyAngle(myResults.get(0));
+				myCharacter.hasUpdated();
+				return result;
+			case SetTowards:
+				double newAngle = 
+					90 - Math.atan2(myResults.get(1) - myCharacter.getCoordY(), myResults.get(0) - myCharacter.getCoordX());
+				result = Math.abs(newAngle - myCharacter.getMyAngle());
+				myCharacter.setMyAngle(newAngle);
+				myCharacter.hasUpdated();
+				return result;
+			case SetPosition:
+				result = findDistance(myResults.get(0), myCharacter.getCoordX(), myResults.get(1), myCharacter.getCoordY());
+				myCharacter.setCurrCoord((int) Math.round(myResults.get(0)), (int) Math.round(myResults.get(1)));
+				myCharacter.hasUpdated();
+				return result;
+			case PenDown:
+				myCharacter.setPenState(true);
+				myCharacter.hasUpdated();
+				return 1;
+			case PenUp:
+				myCharacter.hasUpdated();
+				myCharacter.setPenState(false);
+				return 0;
+			case ShowTurtle:
+				myCharacter.setVisability(true);
+				myCharacter.hasUpdated();
+				return 1;
+			case HideTurtle:
+				myCharacter.setVisability(false);
+				myCharacter.hasUpdated();
+				return 0;
+			case Home:
+				result = findDistance(0, myCharacter.getCoordX(), 0, myCharacter.getCoordY());
+				myCharacter.setCurrCoord(0, 0);
+				myCharacter.hasUpdated();
+				return result;
+			case ClearScreen:
+				//How to do this? for erasing all lines? deleting list?
+				break;
+			case XCoordinate:
+				return myCharacter.getCoordX();
+			case YCoordinate:
+				return myCharacter.getCoordY();
+			case Heading:
+				return myCharacter.getMyAngle();
+			case IsPenDown:
+				return myCharacter.getPenState() ? 1 : 0;
+			case IsShowing:
+				return myCharacter.getVisability() ? 1 : 0;
+			case Sum:
+				for (Double d : myResults) {
+					result += d;
+				}
+				return result;
+			case Difference:
+				result = myResults.get(0);
+				myResults.remove(0);
+				for (Double d : myResults) {
+					result -= d;
+				}
+				return result;
+			case Product:
+				result = myResults.get(0);
+				myResults.remove(0);
+				for (Double d : myResults) {
+					result = result * d;
+				}
+				return result;
+			case Quotient:
+				result = myResults.get(0);
+				myResults.remove(0);
+				for (Double d : myResults) {
+					result = result / d;
+				}
+				return result;
+			case Remainder:
+				result = myResults.get(0);
+				myResults.remove(0);
+				for (Double d : myResults) {
+					result = result % d;
+				}
+				return result;
+			case Minus:
+				return -1 * myResults.get(0);
+			case Random:
+				return Math.floor(Math.random() * myResults.get(0));
+			case Sine:
+				return Math.sin(convertDegrees(myResults.get(0)));
+			case Cosine:
+				return Math.cos(convertDegrees(myResults.get(0)));
+			case Tangent:
+				return Math.tan(convertDegrees(myResults.get(0)));
+			case ArcTangent:
+				//may throw exception here
+				return Math.atan(convertDegrees(myResults.get(0)));
+			case NaturalLog:
+				//may also throw exception here
+				return Math.log(myResults.get(0));
+			case Power:
+				double base = myResults.get(0); 
+				double exp = myResults.get(1);
+				return Math.pow(base, exp);
+			case Pi:
+				return Math.PI;
+			case LessThan:
+				leftValue =  myResults.get(0);
+				rightValue = myResults.get(1);
+				return (leftValue < rightValue ? 1 : 0);
+			case GreaterThan:
+				leftValue =  myResults.get(0);
+				rightValue = myResults.get(1);
+				return (leftValue > rightValue ? 1 : 0);
+			case Equal:
+				leftValue =  myResults.get(0);
+				rightValue = myResults.get(1);
+				return(leftValue == rightValue ? 1 : 0);
+			case NotEqual:
+				leftValue =  myResults.get(0);
+				rightValue = myResults.get(1);
+				return (leftValue != rightValue ? 1 : 0);
+			case And:
+				leftValue =  myResults.get(0);
+				rightValue = myResults.get(1);
+				return (((leftValue != 0) && (rightValue != 0)) ? 1 : 0);
+			case Or:
+				leftValue =  myResults.get(0);
+				rightValue = myResults.get(1);
+				return (((leftValue != 0) || (rightValue != 0)) ? 1 : 0);
+			case Not:
+				return (myResults.get(0) == 0 ? 1 : 0);
+			default:
+				break;
+		}
 		return 0;
 	}
 
