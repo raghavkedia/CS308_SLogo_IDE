6e03bb28154d101f21e89a2e0d1c41e85624197a
Richard Liu <richardliu7896@gmail.com>
2016-02-29 21:01:11 -0500

    added feature to change background color by user

--- src/frontend/ToolbarComponent.java
+++ src/frontend/ToolbarComponent.java
@@ -1,7 +1,13 @@
 package frontend;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 import javafx.geometry.Orientation;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ToolBar;
+import javafx.scene.paint.Color;
 
 public class ToolbarComponent extends VisualComponent{
 	private ToolBar myToolbar;
@@ -9,10 +15,24 @@ public class ToolbarComponent extends VisualComponent{
 	public ToolbarComponent(){
 		myToolbar = new ToolBar();
 		myToolbar.setOrientation(Orientation.HORIZONTAL);
-		myToolbar.getItems().add(ComponentFactory.makeButton("Define a command", 
+		
+		ComboBox<String> colorDropDown = new ComboBox<>();
+		String[] colors = new String[]{"Blue", "White", "Grey", "Red", "Green"};
+		HashMap<String, Color> colorDict = new HashMap<String, Color>();
+		colorDict.put("Blue", Color.BLUE);
+		colorDict.put("White", Color.WHITE);
+		colorDict.put("Grey", Color.GREY);
+		colorDict.put("Red", Color.RED);
+		colorDict.put("Green", Color.GREEN);
+		colorDropDown.getItems().setAll(Arrays.asList(colors));
+		
+		colorDropDown.valueProperty().addListener(
 				e -> {
-					System.out.println("Pressed Define a Command");
-				}));
+					String color = colorDropDown.getSelectionModel().getSelectedItem();
+					FrontendManagerAPI.changeDisplayBackgroundColor(colorDict.get(color));
+				});
+		
+		myToolbar.getItems().add(colorDropDown);
 		super.setVisual(myToolbar);
 	}
 }
