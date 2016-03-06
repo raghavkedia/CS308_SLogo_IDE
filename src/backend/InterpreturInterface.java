9a7e544d14f07e7a23cacdb78c84d90911e4854b
janejzyu <yujiangzhen@hotmail.com>
2016-03-06 18:25:27 -0500

    move file to util

--- src/backend/InterpreturInterface.java
+++ src/backend/InterpreturInterface.java
@@ -17,8 +17,5 @@ public interface InterpreturInterface {
 	public CommandHistory getCommandHistory(int workspaceId);
 	public UserDefinedCommands getUserDefinedCommands(int workspaceId);
 	public Properties getProperties(int workspaceId);
-
-	public void addWorkSpace(int i);
-
-	
+	public void addWorkSpace(int i);	
 }
