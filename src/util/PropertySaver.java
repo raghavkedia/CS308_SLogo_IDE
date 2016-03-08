65853c98afecef3a830956658cf0c1f244e3b71b
janejzyu <yujiangzhen@hotmail.com>
2016-03-07 22:01:32 -0500

    setting saver works

--- src/util/PropertySaver.java
+++ src/util/PropertySaver.java
@@ -8,7 +8,8 @@ import java.util.Properties;
 public class PropertySaver {
 	public static void save(Properties props, String fileName) {
 	    try {
-	    	fileName = PropertyLoader.PATH + fileName + PropertyLoader.EXTENSION;
+	    	fileName = fileName + PropertyLoader.EXTENSION;
+	    	System.out.println(fileName);
 	        File f = new File(fileName);
 	        OutputStream out = new FileOutputStream( f );
 	        props.store(out, "This is an optional header comment string");
